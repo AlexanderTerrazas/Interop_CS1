e433d974f11c1e8a0115457d476eb827cb585c7a
AXEL ANTONIO GARZA MARTINEZ <garza_axel@uadec.edu.mx>
2023-08-27 03:06:34 -0600

    Agregar archivos de proyecto.

---
+++ InteropC/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace InteropC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWord = new System.Windows.Forms.Button();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnWord
+            // 
+            this.btnWord.Location = new System.Drawing.Point(68, 127);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(75, 23);
+            this.btnWord.TabIndex = 0;
+            this.btnWord.Text = "button1";
+            this.btnWord.UseVisualStyleBackColor = true;
+            this.btnWord.Click += new System.EventHandler(this.btnWord_Click_1);
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(160, 127);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnExcel.TabIndex = 1;
+            this.btnExcel.Text = "button2";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click_1);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(68, 70);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(167, 20);
+            this.txtDato.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 222);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.btnWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWord;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.TextBox txtDato;
+    }
+}
+
---
+++ InteropC/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace InteropC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWord_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            if (dialogo.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string ruta = dialogo.FileName;
+            var wordApp = new Word.Application();
+            wordApp.Visible = true;
+            wordApp.Documents.Add();
+            string dato = txtDato.Text;
+
+            wordApp.Selection.TypeText(dato);
+            wordApp.ActiveDocument.SaveAs(ruta);
+        }
+
+        private void btnExcel_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            if (dialogo.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string ruta = dialogo.FileName;
+            var excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            Excel.Workbook workbook = excelApp.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+
+            worksheet.Cells[1, 1] = txtDato.Text;
+            workbook.SaveAs(ruta);
+        }
+    }
+}
